8217f418b27adb05c4f4908ba612e73e771da7ef
diasnf <leonamfernandes@hotmail.com>
2019-01-30 13:39:24 -0200

    Update iposprinter.ts

--- lib/printers/iposprinter/iposprinter.ts
+++ lib/printers/iposprinter/iposprinter.ts
@@ -31,15 +31,13 @@ export default class IposPrinter extends BufferBuilder {
     }
 
     public printBarcode(data: string, barcodeSystem: number, width: number = this.command.BARCODE_WIDTH, height: number = 162, labelFont: number = this.command.BARCODE_LABEL_FONT, labelPosition: number = this.command.BARCODE_LABEL_POSITION, leftSpacing: number = 0): BufferBuilder {        
-        /*this.startAlign(ALIGNMENT.CENTER);
+        this.startAlign(ALIGNMENT.CENTER);
         this.buffer.write(this.command.GS_h(100)); // height        
         this.buffer.write(this.command.GS_H(labelPosition)); // HRI font
         barcodeSystem = barcodeSystem = 72 ? 73 : barcodeSystem;
         this.buffer.write(this.command.GS_K(barcodeSystem, data.length)); // data is a string in UTF-8        
         this.buffer.write(data, 'ascii');        
         this.resetAlign();    
-        return this;*/
-        this.printQRcode(data);
         return this;
       }
     
