4e692d86c57a449c9a25c9155f557186daf56a42
MatheusFrez <matheusfrez.nf@gmail.com>
2019-10-18 18:06:40 -0300

    Acerto alinhamento

--- lib/printers/innerprinter/innerprinter.ts
+++ lib/printers/innerprinter/innerprinter.ts
@@ -11,7 +11,7 @@ export default class InnerPrinter extends BufferBuilder {
     printBarcode(data: string, barcodeSystem: number, width: number = this.command.BARCODE_WIDTH, height: number = 162, labelFont: number = this.command.BARCODE_LABEL_FONT, labelPosition: number = this.command.BARCODE_LABEL_POSITION, leftSpacing: number = 0): InnerPrinter {
         this.buffer.write(this.command.GS_w(width)); // width
         this.buffer.write(this.command.GS_h(100)); // height
-        //this.buffer.write(this.command.GS_L(60, 0)); // left spacing
+        //run buthis.buffer.write(this.command.GS_L(60, 0)); // left spacing
         this.buffer.write(this.command.GS_f(labelFont)); // HRI font
         this.buffer.write(this.command.GS_H(labelPosition)); // HRI font
         this.buffer.write(this.command.GS_K(barcodeSystem, data.length)); // data is a string in UTF-8
